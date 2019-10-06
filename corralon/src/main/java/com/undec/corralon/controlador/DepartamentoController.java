e97e95f3a2fc81d66f9a74cc37e9d84d3f8eae5e
Himura Kenji <aaron.ariperto@gmail.com>
2019-10-06 18:15:49 -0300

    Controller: Se completo ABM DepartamentoController, falta CrossOrigin

---
+++ corralon/src/main/java/com/undec/corralon/controlador/DepartamentoController.java
@@ -0,0 +1,55 @@
+package com.undec.corralon.controlador;
+
+import com.undec.corralon.DTO.Response;
+import com.undec.corralon.modelo.Departamento;
+import com.undec.corralon.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/departamentos")
+public class DepartamentoController {
+
+    @Autowired
+    DepartamentoService departamentoService;
+
+    @GetMapping
+    public ResponseEntity<Response> listarTodos() throws Exception {
+        Response response = departamentoService.listarTodos();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/habilitado")
+    public ResponseEntity<Response> listarHabilitados() throws Exception {
+        Response response = departamentoService.listarTodosHabilitados();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> listarPorId(@PathVariable("id") Integer id) throws Exception {
+        Response response = departamentoService.listarPorId(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Response> guardar(@Valid @RequestBody Departamento departamento) throws Exception {
+        Response response = departamentoService.guardar(departamento);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Response> actualizar(@Valid @RequestBody Departamento departamento) throws Exception {
+        Response response = departamentoService.guardar(departamento);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> eliminar(@PathVariable("id") Integer id) throws Exception {
+        Response response = departamentoService.darDeBaja(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
