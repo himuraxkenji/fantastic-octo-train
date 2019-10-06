046d01d407c7e2a241a37c5ec296d8eabff98c84
Himura Kenji <aaron.ariperto@gmail.com>
2019-10-06 18:04:26 -0300

    Repository: Se añadio metodo que devuelve departamentos habilitados

--- corralon/src/main/java/com/undec/corralon/repository/DepartamentoRepository.java
+++ corralon/src/main/java/com/undec/corralon/repository/DepartamentoRepository.java
@@ -4,6 +4,9 @@ import com.undec.corralon.modelo.Departamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
+    List<Departamento> findAllByHabilitadoEquals(Integer habilitado);
 }
