276afc545ea275f0285b0e190eeb7a7f31245e03
Himura Kenji <aaron.ariperto@gmail.com>
2019-10-21 15:42:07 -0300

    Frontend: Menu y Submenu Fijo

--- frontCorralon/src/app/ventas/sub-menu/sub-menu.component.ts
+++ frontCorralon/src/app/ventas/sub-menu/sub-menu.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sub-menu',
   templateUrl: './sub-menu.component.html',
-  styles: []
+  styles: ['./sub-menu.component.css']
 })
 export class SubMenuComponent implements OnInit {
 
